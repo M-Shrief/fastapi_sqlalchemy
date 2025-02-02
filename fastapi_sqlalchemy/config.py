c505affc1d05fc80337b16c79d6c427578b06849
m-shrief <m.shrief.dev@proton.me>
2025-02-02 11:41:44 +0200

    Added Basic utils for Authentication using JWT.

--- fastapi_sqlalchemy/config.py
+++ fastapi_sqlalchemy/config.py
@@ -11,3 +11,8 @@ DB = {
     'url': F"postgresql://{__env.get("DB_USER")}:{__env.get("DB_PASSWORD")}@{__env.get("DB_HOST")}:{__env.get("DB_PORT")}/{__env.get("DB_NAME")}",
     'conn_str':  F"host={__env.get("DB_HOST")} port={__env.get("DB_PORT")} user={__env.get("DB_USER")} dbname={__env.get("DB_NAME")} password={__env.get("DB_PASSWORD")} sslmode=disable"
 }
+
+JWT = {
+    "private": open("jwtRSA256-private.pem").read(),
+    "public": open("jwtRSA256-public.pem").read()
+}
--- fastapi_sqlalchemy/utils/auth.py
+++ fastapi_sqlalchemy/utils/auth.py
@@ -1,8 +1,48 @@
 import bcrypt
+import jwt
+from typing import Any
+from datetime import UTC, datetime, timedelta
+import json
+### 
+from fastapi_sqlalchemy.config import JWT
+from fastapi_sqlalchemy.components.users import schema
+
 def hash_password(password: str) -> str:
     hashed_password: bytes = bcrypt.hashpw(password.encode(), bcrypt.gensalt(rounds=12))
     return hashed_password.decode()
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(password.encode(), hashed_password.encode())
-    # return checkpw(password.encode(), hashed_password.encode())
+
+
+def create_access_token(user: schema.UserBaseRes)->str:
+    payload = {
+        "user": {
+            "id": str(user.id),
+            "name": user.name,
+            "roles": user.roles
+            }
+    }
+    time: datetime = datetime.now(UTC).replace(tzinfo=None)
+    payload["iat"]= time # Issued at {time}
+    payload["exp"]= time + timedelta(minutes= 60 * 2) # Expire after 2 hours
+
+    token: str = jwt.encode(
+        payload=payload,
+        key=JWT["private"],
+        algorithm="RS256"
+        )
+
+    return token
+
+def verify_access_token(token: str)->(Any | None, bool):
+    try:
+        payload = jwt.decode(
+            token=token,
+            key=JWT["public"],
+            algorithms=["RS256"]
+            )
+
+        return payload, True
+    except jwt.DecodeError:
+        return None, False
