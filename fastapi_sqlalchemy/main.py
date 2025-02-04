312531c3c2cf670b163cbe51190a0bf713800e16
m-shrief <m.shrief.dev@proton.me>
2025-02-04 14:54:55 +0200

    Added CORS and GZIP middlewares.

--- fastapi_sqlalchemy/main.py
+++ fastapi_sqlalchemy/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 ###
 from fastapi_sqlalchemy.database.index import lifespan
 from fastapi_sqlalchemy.components.users.router import router as user_router
 
 app = FastAPI(lifespan=lifespan)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    GZipMiddleware,
+    minimum_size=1000,
+    compresslevel=5
+)
 
 @app.get("/")
 async def homepage():
